63782319fd59ea5cf99332f40827788d247f2cfa
Harvey <sylv1612@stud.kea.dk>
2022-09-22 12:47:31 +0200

    clean state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,38 +29,8 @@ const Dashboard = () => {
         <Grid item xs={12} md={4}>
           <Trophy />
         </Grid>
-        <Grid item xs={12} md={8}>
-          <StatisticsCard />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <WeeklyOverview />
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <TotalEarning />
-        </Grid>
         <Grid item xs={12} md={6} lg={4}>
           <Grid container spacing={6}>
-            <Grid item xs={6}>
-              <CardStatisticsVerticalComponent
-                stats='$25.6k'
-                icon={<Poll />}
-                color='success'
-                trendNumber='+42%'
-                title='Total Profit'
-                subtitle='Weekly Profit'
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <CardStatisticsVerticalComponent
-                stats='$78'
-                title='Refunds'
-                trend='negative'
-                color='secondary'
-                trendNumber='-15%'
-                subtitle='Past Month'
-                icon={<CurrencyUsd />}
-              />
-            </Grid>
             <Grid item xs={6}>
               <CardStatisticsVerticalComponent
                 stats='862'
@@ -84,15 +54,10 @@ const Dashboard = () => {
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <SalesByCountries />
+        <Grid item xs={12} md={12} lg={12}>
+          <WeeklyOverview />
         </Grid>
-        <Grid item xs={12} md={12} lg={8}>
-          <DepositWithdraw />
-        </Grid>
-        <Grid item xs={12}>
-          <Table />
-        </Grid>
+
       </Grid>
     </ApexChartWrapper>
   )
