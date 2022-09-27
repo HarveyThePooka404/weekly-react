b2b85a1f9662f86548ef15019c1fa5c5763b8c09
Harvey <sylv1612@stud.kea.dk>
2022-09-27 10:36:17 +0200

    tags working

--- src/views/pills/ChipInput.tsx
+++ src/views/pills/ChipInput.tsx
@@ -1,46 +1,74 @@
 import Chip from '@mui/material/Chip'
 import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
-import { getToken } from 'next-auth/jwt'
-import { User } from '@prisma/client'
-import prisma from 'src/lib/prisma';
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
-export default function Tags({ activities }: { activities: string[] }) {
+export default function Tags({
+  activities,
+  doneDuringTheDay,
+  updateDay
+}: {
+  activities: string[]
+  doneDuringTheDay: string[]
+  updateDay: any
+}) {
+  const router = useRouter()
+  const refreshData = () => {
+    router.push(router.asPath)
+  }
+
+  const [activitiesToShow, addActivity] = useState(activities)
+
+  const updateTag = (event: any, value: string[]) => {
+    const target = event.target as HTMLInputElement
+    if (typeof target.value == 'string') {
+      addNewTagsToUser(target.value)
+      addActivity(activitiesToShow => [...activitiesToShow, target.value])
+    }
+
+    updateDay.function(updateDay.dayId, 'activities', value)
+  }
+
   return (
     <Autocomplete
-    multiple
-    id='tags-filled'
-    options={activities.map(option => option)}
-    freeSolo
-    renderTags={(value: readonly string[], getTagProps) =>
-      value.map((option: string, index: number) => (
-        <Chip variant='outlined' label={option} {...getTagProps({ index })} />
+      multiple
+      id='tags-filled'
+      options={activitiesToShow.map(option => option)}
+      freeSolo
+      onChange={updateTag}
+      defaultValue={doneDuringTheDay}
+      renderTags={(value: readonly string[], getTagProps) =>
+        value.map((option: string, index: number) => (
+          <Chip variant='outlined' label={option} {...getTagProps({ index })} />
         ))
       }
       renderInput={params => <TextField {...params} label='Activities' placeholder='Favorites' />}
-      />
-      )
+    />
+  )
 }
 
-async function updateDay(userId: string, activitiesToCreate: string[]) {
+async function addNewTagsToUser(tag: string) {
+  await updateUser(tag)
+}
+
+async function updateUser(activitiesToAdd: string) {
   const data = {
-    id: id,
-    quality: quality,
-    status: 'DONE'
+    activities: activitiesToAdd
   }
-  
-  const JSONdata = JSON.stringify(data) 
 
-  const endpoint = '/api/updateDay'
+  const JSONdata = JSON.stringify(data)
+
+  const endpoint = '/api/updateUser'
   const options = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      }, 
-      body: JSONdata
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSONdata
   }
 
-  const response = await fetch(endpoint, options);
-  const result = await response.json();
+  const response = await fetch(endpoint, options)
+  const result = await response.json()
   return result
 }
